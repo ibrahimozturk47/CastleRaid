6a1d3be302fd511a38f3f19684df50bae03b9d97
İbrahim Halil Öztürk <95483989+ibrahimozturk47@users.noreply.github.com>
2023-10-15 12:21:32 +0300

    Second Commit

--- Assets/pushscripts/cube.cs
+++ Assets/pushscripts/cube.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class cube : MonoBehaviour
 {
   
     public Collider collider;
+    public GameObject swordP,panelP;
+    
+
     Rigidbody rb;
     Animator animator;
     public GameObject sword;
+    bool panelPB=false;
     // Start is called before the first frame update
     void Start()
     {
+        panelP.SetActive(false);
        rb = GetComponent<Rigidbody>();
        animator = GetComponent<Animator>();    
     }
@@ -19,13 +25,38 @@ public class cube : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (sword.GetComponent<sword>().dead >= 50)
+        if (sword.GetComponent<sword>().dead >= 50 && panelPB==false)
         {
             animator.SetInteger("animEn",1);
             
             collider.enabled=false;
+
+            panelP.SetActive(true);
         }
+
+        if(Input.GetKey(KeyCode.F))
+        {
+            panelP.SetActive(false);
+            panelPB = true;
+        }
+
         
 
     }
+   
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetInteger("animEn", 2);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetInteger("animEn", 0);
+        }
+    }
+
 }
